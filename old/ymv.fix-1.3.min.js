a82911ad3c562b031959ad89c501928b78cb878b
yordanny90 <yordanny90@gmail.com>
2016-11-30 08:17:22 -0600

    Add files via upload

---
+++ old/ymv.fix-1.3.min.js
@@ -0,0 +1,8 @@
+/*
+ * Javascript Fix Console
+ * ymv.fix v1.3
+ * Fecha: 20161121
+ * Autor: Yordanny Mejías V. yordanny90@gmail.com
+ */
+//	Crea el objeto console, y agrega las funciones faltantes para evitar errores en browsers que no soportan este objeto
+$(function(a){"object"!=typeof console&&(console=new Object),$joq(console).insert({debug:function(){},error:function(){},info:function(){},log:function(){},warn:function(){},dir:function(){},dirxml:function(){},group:function(){},groupCollapsed:function(){},groupEnd:function(){},clear:function(){},count:function(){},assert:function(){},profile:function(){},profileEnd:function(){},time:function(){},timeEnd:function(){},timeStamp:function(){}})});
